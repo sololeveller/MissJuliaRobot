98e579bdecf8ca31dfb0bdf5654b293aa3931864
MissJuliaRobot <54566209+MissJuliaRobot@users.noreply.github.com>
2020-10-30 14:34:44 +0530

    Update misc.py

--- julia/modules/misc.py
+++ julia/modules/misc.py
@@ -4071,56 +4071,56 @@ async def sticklet(event):
         await event.reply("I only understand by on or off")
         return
 
+import nude
+from nude import Nude
 
-@register(pattern="")
+@register(patteen="")      
 async def spam_update(event):
-    if not event:
-        return
-    if event.fwd_from:
-        return
-    if event.is_private:
-        return
-    if MONGO_DB_URI is None:
-        return
-    sender = await event.get_sender()
-    chats = spammers.find({})
-    for c in chats:
-        if event.chat_id == c["id"]:
-            if event.is_group:
-              if await is_register_admin(event.chat_id, event.from_id):
-                  return
-              elif str(event.from_id) in str(OWNER_ID):
-                  return
-              pass
-
-              if event.text:
-                msg = str(event.text)
-                if profanity.contains_profanity(msg):
-                    await event.delete()
-                    if sender.username is None:
-                        st = sender.first_name
-                        hh = sender.id
-                        final = f"[{st}](tg://user?id={hh}) **{msg}** is detected as a slang word and your message has been deleted"
-                    else:
-                        let = sender.username
-                        final = f"@{let} **{msg}** is detected as a slang word and your message has been deleted"
-                    dev = await event.respond(final)
-                    await asyncio.sleep(10)
-                    await dev.delete()
-              if event.text:
-                await event.client.download_media(event.photo, "nudes.jpg")
-                if nude.is_nude("./nudes.jpg"):
-                    await event.delete()
-                    if sender.username is None:
-                        st = sender.first_name
-                        hh = sender.id
-                        final = f"[{st}](tg://user?id={hh}) your message has been deleted due to pornographic content"
-                    else:
-                        final = f"@{let} your message has been deleted due to pornographic content"
-                    dev = await event.respond(final)
-                    await asyncio.sleep(10)
-                    await dev.delete()
-                    os.remove("nudes.jpg")
+  if event.fwd_from:
+    return  
+  if event.is_private:  	
+   return
+  if MONGO_DB_URI is None:
+   return
+  msg = str(event.text)
+  sender = await event.get_sender()
+  let = sender.username
+  if event.is_group:
+    if (await is_register_admin(event.input_chat, event.message.sender_id)):
+       return
+    else:
+       pass     
+  chats = spammers.find({})
+  for c in chats:
+   if event.text: 
+    if event.chat_id == c['id']:
+     if profanity.contains_profanity(msg) == True:
+        await event.delete()
+        if sender.username == None:
+           st = sender.first_name
+           hh = sender.id
+           final = f"[{st}](tg://user?id={hh}) **{msg}** is detected as a slang word and your message has been deleted"
+        else:
+           final = f'@{let} **{msg}** is detected as a slang word and your message has been deleted'
+        dev = await event.respond(final)
+        await asyncio.sleep(10)
+        await dev.delete()
+   if event.photo:
+     if event.chat_id == c['id']:
+        await event.client.download_media(event.photo, "nudes.jpg")
+        if nude.is_nude('./nudes.jpg') == True:
+           await event.delete()
+           if sender.username == None:
+             st = sender.first_name
+             hh = sender.id
+             final = f"[{st}](tg://user?id={hh}) your message has been deleted due to pornographic content"
+           else:
+             final = f'@{let} your message has been deleted due to pornographic content'
+           dev = await event.respond(final)
+           await asyncio.sleep(10)
+           await dev.delete()
+           os.remove("nudes.jpg")
+    
 
 
 @register(pattern="^/eval")
