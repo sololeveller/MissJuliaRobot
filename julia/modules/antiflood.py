a688e59755b4833b1d022c8335fc515980a6478d
MissJuliaRobot <54566209+MissJuliaRobot@users.noreply.github.com>
2020-11-01 01:53:36 +0530

    Update antiflood.py

--- julia/modules/antiflood.py
+++ julia/modules/antiflood.py
@@ -673,6 +673,7 @@ from telegram.ext import Filters
 from telegram.ext import MessageHandler
 from telegram.ext import run_async
 from telegram.utils.helpers import mention_html
+from julia.modules.helper_funcs.chat_status import spamcheck
 
 from julia import dispatcher
 from julia.modules.helper_funcs.alternate import send_message
